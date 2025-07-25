45f81d94ccfa0882a7367dbed83fcb7eed3af993
ruchika <ruchika.metkar@gmail.com>
2025-07-25 23:24:37 +0530

    Delete templates/utils directory

--- templates/utils/predict.py
+++
@@ -1,7 +0,0 @@
-
-import random
-
-def predict_from_video(video_path):
-    # Placeholder function
-    gestures = ['Hello', 'Yes', 'No', 'Thanks', 'Please', 'Help']
-    return random.choice(gestures)
