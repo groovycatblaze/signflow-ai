eb19f09c64fe7da24f8b297bcfcf8c79584d0250
ruchika <ruchika.metkar@gmail.com>
2025-07-25 23:25:24 +0530

    Create predict.py

---
+++ utils/predict.py
@@ -0,0 +1,7 @@
+
+import random
+
+def predict_from_video(video_path):
+    # Placeholder function
+    gestures = ['Hello', 'Yes', 'No', 'Thanks', 'Please', 'Help']
+    return random.choice(gestures)
